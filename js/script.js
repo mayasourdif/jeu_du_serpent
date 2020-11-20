cddc7d5fcb791ed74652c0c27e82939ba13f67fa
maya sourdif <mayasourdif@yahoo.ca>
2020-11-20 10:59:16 -0500

    Nouvelle Partie

--- js/script.js
+++ js/script.js
@@ -3,8 +3,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 //le jeu
     class Jeu{
-        constructor() {
+        constructor(_idSvg, _idPointage) {//mis une _ pour comprendre cest des parametres , pas se melanger
             console.log("Création du jeu");
+            this.s= Snap(_idSvg);
+            this.sortiePointage = document.querySelector(_idPointage);
+            this.grandeurCarre=20;//grandeur des carré des serpents
+            this.grandeurGrille=15;//grandeur deu nb de carrés pour la grille complete
+
+        }
+        nouvellePartie(){
+            this.affichagePointage(1);
+
+            this.pomme= new Pomme;
+            this.serpent= new Serpent;
+            // this.*** : ***= une propriété
+
+        }
+
+        finPartie(){
+
+        }
+
+        affichagePointage(_lePointage){
+            this.sortiePointage.innerHTML = _lePointage;
         }
     }
 
@@ -23,8 +44,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
-    var unePartie = new Jeu();//Création de l'objet Jeu avec la description de la class jeu
+    var unePartie = new Jeu("#jeu", "#pointage");//Création de l'objet Jeu avec la description de la class jeu
+//les # c'est pcq cest des id dans le html
+    var btnJouer = document.querySelector("#btnJouer");
+    btnJouer.addEventListener("click", nouvellePartie);
 
+    function nouvellePartie() {
+        unePartie.nouvellePartie();//.nouvellePartie fait référence à la nouvelle partie en haut
+    }
 });
 
 //une class c'est la déclaration d'un objet pas sa création
