6796dfbfe9bf5edb7519a90acece63d81f49d937
maya sourdif <mayasourdif@yahoo.ca>
2020-11-20 10:44:53 -0500

    Création Jeu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+//le jeu
+    class Jeu{
+        constructor() {
+            console.log("Création du jeu");
+        }
+    }
+
+
+// le serpent
+    class Serpent{
+        constructor() {
+            console.log("Création du serpent")
+        }
+    }
+
+//la pomme
+    class Pomme{
+        constructor() {
+            console.log("Création de la pomme")
+        }
+    }
+
+    var unePartie = new Jeu();//Création de l'objet Jeu avec la description de la class jeu
 
 });
+
+//une class c'est la déclaration d'un objet pas sa création
